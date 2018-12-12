4829ba258dfb3d27bfcd22311b02e7691569e4a9
josebalaguer2013@gmail.com <josebalaguer2013@gmail.com>
2018-12-12 19:43:14 -0400

    defini el titulo principal de laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel curso balaguer
+                    Laravel curso Jose Balaguer
                 </div>
 
                 <div class="links">
